8b8a9d2171d65ea40b0dced5c54ad3eaeb218e99
Carole Desiree Cachero <cdcachero@myseneca.ca>
2024-05-15 11:36:55 -0400

    added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Carole Desiree Cachero
+#Program Name: lab1d.py
+#Date Created: 05-15-2024
+#Purpose: Perform Math operations
+
+x = 10
+y = 2
+z = 5
+
+print(str(x) + ' + ' +  str(y) + ' * ' +  str(z) + ' = ' + str(x+y*z))
