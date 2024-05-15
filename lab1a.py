8793921f08d6e028dd4daf90a2bfb6b08ae45248
Carole Desiree Cachero <cdcachero@myseneca.ca>
2024-05-14 22:14:22 -0400

    Uploaded lab1a

---
+++ lab1a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Author: Carole Desiree Cachero
+#Title: OPS445NAA-Lab1
+#Program Name: lab1a.py
+#Purpose: Use the print() function and print 'hello world' to the screen
+#Date Created: 05-14-2024
+
+print('Hello world')
