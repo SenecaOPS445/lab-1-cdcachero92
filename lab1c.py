da08c76e2e40e588b3381ce42614abd5a77fa50f
Carole Desiree Cachero <cdcachero@myseneca.ca>
2024-05-15 11:26:21 -0400

    added lab1c

---
+++ lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Carole Desiree Cachero
+#Program Name: lab1c.py
+#Date Created: 05-15-2024
+#Purpose: To demonstrate how to use Integer Objects
+
+name = 'Isaac'
+age = 72
+
+
+print(name + ' is ' + str(age) + ' years old!')
