e77d1a0073d4c6c82e747077476948aa9ce8a612
Carole Desiree Cachero <cdcachero@myseneca.ca>
2024-05-15 11:09:06 -0400

    Added lab1b.py

---
+++ lab1b.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Carole Desiree Cachero
+#Program Name: lab1b.py
+#Date Created: 05-15-2024
+#Purpose: To demonstrate how to use string objects
+
+
+name = 'Isaac'
+
+print('How old are you ' + name + '?')
